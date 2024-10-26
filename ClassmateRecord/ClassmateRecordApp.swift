52834ef07400ea0838e9d7c4a8945cdf2807ba77
gadfly1981 <gadfly1981@gmail.com>
2024-10-26 19:55:16 +0800

    Initial Commit

---
+++ ClassmateRecord/ClassmateRecordApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClassmateRecordApp.swift
+//  ClassmateRecord
+//
+//  Created by 汪昱 on 2024/10/26.
+//
+
+import SwiftUI
+
+@main
+struct ClassmateRecordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClassmateRecord/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ClassmateRecord
+//
+//  Created by 汪昱 on 2024/10/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
